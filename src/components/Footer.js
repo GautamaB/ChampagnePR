d87e4d2a9e0158fa75ee649d9ae4fd185984ed4e
Thib <thibaud.royer051@gmail.com>
2018-12-20 16:55:33 +0100

    add google maps

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Form from "./Form";
+import GoogleMap from "./GoogleMap";
+
+import { Row, Col } from "reactstrap";
 
 const footer = () => {
   return (
@@ -19,20 +22,29 @@ const footer = () => {
         }}
       />
       <p style={{ border: "3px solid #D5B23C" }} />
-      <div style={{ paddingBottom: "65px" }} id="Contact" />
-      <h1 style={{ marginLeft: "15px" }}>Contact : </h1>
-      <p style={{ marginLeft: "35px" }}>
-        REGNAULT PATRICK
-        <br />
-        45 Grande rue
-        <br />
-        51190 Le Mesnil sur Oger
-        <br />
-        Téléphone : 03 26 57 54 50
-        <br />
-        Email : champagne-p-regnault@orange.fr
-      </p>
-      <Form />
+      <Row>
+        <Col>
+          <div style={{ paddingBottom: "65px" }} id="Contact" />
+          <h1 style={{ marginLeft: "15px" }}>Contact : </h1>
+          <p style={{ marginLeft: "35px" }}>
+            REGNAULT PATRICK
+            <br />
+            45 Grande rue
+            <br />
+            51190 Le Mesnil sur Oger
+            <br />
+            Téléphone : 03 26 57 54 50
+            <br />
+            Email : champagne-p-regnault@orange.fr
+          </p>
+          <Form />
+        </Col>
+        <Col>
+          <div style={{ marginTop: "85px" }}>
+            <GoogleMap />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
---
+++ src/components/GoogleMap.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        style={{ width: "80%", height: "500px" }}
+        initialCenter={{
+          lat: 48.947992,
+          lng: 4.025
+        }}
+        zoom={16}
+        onClick={this.onMapClicked}
+      >
+        <Marker onClick={this.onMarkerClick} name={"champagne"} />
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>
+            <h1>home</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCAVTFa2l7dyaYUEmHfvXTVnMcYF8JVqI8"
+})(MapContainer);
